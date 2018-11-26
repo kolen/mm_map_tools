5627f79ba638a51f01a53b1d10e417882ed21c23
kolen <incredible.angst@gmail.com>
2018-11-26 21:59:42 +0300

    Add tile_at

--- src/map.rs
+++ src/map.rs
@@ -7,6 +7,10 @@ pub struct Map {
     contents: Vec<u8>,
 }
 
+pub struct Tile {
+    id: u16,
+}
+
 impl Map {
     pub fn from_contents(contents: Vec<u8>) -> Self {
         assert_eq!(6, LittleEndian::read_u32(&contents));
@@ -16,6 +20,22 @@ impl Map {
             size_z: LittleEndian::read_u32(&contents[0xc..]),
             contents: contents,
         }
+    }
+
+    pub fn tile_at(&self, x: u32, y: u32, z: u32) -> Tile {
+        assert!(x < self.size_x);
+        assert!(y < self.size_y);
+        assert!(z < self.size_z);
+        let offset: usize = self.size_z as usize * self.size_y as usize * 6 * z as usize
+            + self.size_y as usize * 6 * y as usize
+            + x as usize * 6;
+        Tile {
+            id: LittleEndian::read_u16(&self.tiles_data()[offset..]),
+        }
+    }
+
+    fn tiles_data(&self) -> &[u8] {
+        &self.contents[0x4c..]
     }
 }
 
@@ -31,5 +51,6 @@ mod tests {
         assert_eq!(20, map.size_x);
         assert_eq!(20, map.size_y);
         assert_eq!(24, map.size_z);
+        assert_eq!(878, map.tile_at(10, 10, 20).id);
     }
 }
