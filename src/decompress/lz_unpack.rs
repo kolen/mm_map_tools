fba5aa22631d073870367f30e3193c11aa50cf8f
kolen <incredible.angst@gmail.com>
2019-01-17 22:51:50 +0300

    Rename bit length specifier on number literals

--- src/decompress/lz_unpack.rs
+++ src/decompress/lz_unpack.rs
@@ -22,8 +22,8 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
 
     let lz = &mut lz_input;
     (*lz).ptr = input;
-    (*lz).bit_ptr = 0x80i32 as u8;
-    (*lz).value = 0i32 as u32;
+    (*lz).bit_ptr = 0x80 as u8;
+    (*lz).value = 0 as u32;
     let mut count = 0;
     let mut count_save = 0;
     let mut dict_index: i32 = 1;
@@ -31,7 +31,7 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
         let mut value: i8;
         let mut ptr_inc: *const u8;
         let mut bit: u8 = (*lz).bit_ptr;
-        if bit as i32 == 0x80i32 {
+        if bit as i32 == 0x80 {
             unsafe {
                 ptr_inc = (*lz).ptr.offset(1isize);
                 (*lz).value = *(*lz).ptr as u32;
@@ -39,17 +39,17 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
             }
         }
         let value_bit: i32 = ((*lz).value & bit as u32) as i32;
-        let next_bit: i8 = (bit as i32 >> 1i32) as i8;
+        let next_bit: i8 = (bit as i32 >> 1) as i8;
         (*lz).bit_ptr = next_bit as u8;
         if 0 == next_bit {
-            (*lz).bit_ptr = 0x80i32 as u8
+            (*lz).bit_ptr = 0x80 as u8
         }
         if 0 != value_bit {
-            let mut high_bit: u32 = 0x80i32 as u32;
-            value = 0i32 as i8;
+            let mut high_bit: u32 = 0x80 as u32;
+            value = 0 as i8;
             loop {
                 bit = (*lz).bit_ptr;
-                if bit as i32 == 0x80i32 {
+                if bit as i32 == 0x80 {
                     unsafe {
                         ptr_inc = (*lz).ptr.offset(1isize);
                         (*lz).value = *(*lz).ptr as u32;
@@ -59,11 +59,11 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
                 if 0 != bit as u32 & (*lz).value {
                     value = (value as u32 | high_bit) as i8
                 }
-                high_bit >>= 1i32;
-                let next_bit_2: i8 = (bit as i32 >> 1i32) as i8;
+                high_bit >>= 1;
+                let next_bit_2: i8 = (bit as i32 >> 1) as i8;
                 (*lz).bit_ptr = next_bit_2 as u8;
                 if 0 == next_bit_2 {
-                    (*lz).bit_ptr = 0x80i32 as u8
+                    (*lz).bit_ptr = 0x80 as u8
                 }
                 if !(0 != high_bit) {
                     break;
@@ -77,13 +77,13 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
             count += 1;
             lz_dict[dict_index as usize] = value as u8;
             count_save = count;
-            dict_index = dict_index as u16 as i32 + 1i32 & 0xfffi32
+            dict_index = dict_index as u16 as i32 + 1 & 0xfff
         } else {
-            let mut back_ref_bit: u32 = 0x800i32 as u32;
+            let mut back_ref_bit: u32 = 0x800;
             let mut back_ref_off: i32 = 0;
             loop {
                 bit = (*lz).bit_ptr;
-                if bit as i32 == 0x80i32 {
+                if bit as i32 == 0x80 {
                     unsafe {
                         ptr_inc = (*lz).ptr.offset(1isize);
                         (*lz).value = *(*lz).ptr as u32;
@@ -93,11 +93,11 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
                 if 0 != bit as u32 & (*lz).value {
                     back_ref_off = (back_ref_off as u32 | back_ref_bit) as i32
                 }
-                back_ref_bit >>= 1i32;
-                let next_bit_3: i8 = (bit as i32 >> 1i32) as i8;
+                back_ref_bit >>= 1;
+                let next_bit_3: i8 = (bit as i32 >> 1) as i8;
                 (*lz).bit_ptr = next_bit_3 as u8;
                 if 0 == next_bit_3 {
-                    (*lz).bit_ptr = 0x80i32 as u8
+                    (*lz).bit_ptr = 0x80 as u8
                 }
                 if !(0 != back_ref_bit) {
                     break;
@@ -110,7 +110,7 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
             let mut back_ref_len: i32 = 0;
             loop {
                 bit = (*lz).bit_ptr;
-                if bit as i32 == 0x80i32 {
+                if bit as i32 == 0x80 {
                     unsafe {
                         ptr_inc = (*lz).ptr.offset(1isize);
                         (*lz).value = *(*lz).ptr as u32;
@@ -121,18 +121,18 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
                 if 0 != bit as u32 & (*lz).value {
                     back_ref_len = (back_ref_len as u32 | low_bit) as i32
                 }
-                low_bit >>= 1i32;
-                let next_bit_4: i8 = (bit as i32 >> 1i32) as i8;
+                low_bit >>= 1;
+                let next_bit_4: i8 = (bit as i32 >> 1) as i8;
                 (*lz).bit_ptr = next_bit_4 as u8;
                 if 0 == next_bit_4 {
-                    (*lz).bit_ptr = 0x80i32 as u8
+                    (*lz).bit_ptr = 0x80 as u8
                 }
                 if !(0 != low_bit) {
                     break;
                 }
             }
-            let mut back_ref_i: i32 = 0i32;
-            if back_ref_len + 1i32 >= 0i32 {
+            let mut back_ref_i: i32 = 0;
+            if back_ref_len + 1 >= 0 {
                 loop {
                     let value_2 = lz_dict
                         [(back_ref_off as u16 as i32 + back_ref_i as u16 as i32 & 0xfffi32) as usize]
@@ -149,8 +149,8 @@ pub fn lz_unpack(input: *const u8, mut output: *mut u8, unpacked_size: usize) {
                     }
                     lz_dict[dict_index as usize] = value_2 as u8;
                     back_ref_i += 1;
-                    dict_index = dict_index as u16 as i32 + 1i32 & 0xfffi32;
-                    if !(back_ref_i < back_ref_len + 2i32) {
+                    dict_index = dict_index as u16 as i32 + 1 & 0xfffi32;
+                    if !(back_ref_i < back_ref_len + 2) {
                         break;
                     }
                 }
