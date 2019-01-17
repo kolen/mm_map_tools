033d3c04b4a5f1f9f1da029b10bdd1748d15f29d
kolen <incredible.angst@gmail.com>
2019-01-18 02:45:20 +0300

    Clean up prng_init arguments

--- src/decompress/deobfuscate.rs
+++ src/decompress/deobfuscate.rs
@@ -31,7 +31,7 @@ unsafe fn prng(mut table: *mut u32) -> u32 {
     return c;
 }
 
-pub unsafe fn prng_init(mut table: *mut u32, mut seed: u32) {
+fn prng_init(table: &mut [u32; 256], mut seed: u32) {
     let mut p: *mut u32 = 0 as *mut u32;
     let mut i: i32 = 0;
     let mut count: i32 = 0;
@@ -40,36 +40,49 @@ pub unsafe fn prng_init(mut table: *mut u32, mut seed: u32) {
     let mut k: u32 = 0;
     let mut q: *mut i32 = 0 as *mut i32;
     let mut b: i32 = 0;
-    if 0 == PRNG_MapReady {
-        i = 0i32;
-        while i < 0xf9i32 {
-            PRNG_Map[i as usize] = (i + 1i32) as u32;
-            i += 1
+
+    unsafe {
+        if 0 == PRNG_MapReady {
+            i = 0i32;
+            while i < 0xf9i32 {
+                PRNG_Map[i as usize] = (i + 1i32) as u32;
+                i += 1
+            }
+            PRNG_Map[0xf9i32 as usize] = 0i32 as u32
         }
-        PRNG_Map[0xf9i32 as usize] = 0i32 as u32
+        PRNG_State = seed;
     }
-    PRNG_State = seed;
-    *table.offset(0isize) = 0i32 as u32;
-    *table.offset(1isize) = 103i32 as u32;
-    p = table.offset(251isize);
-    count = 250i32;
-    i = 0i32;
-    while i < 250i32 {
-        t = 0x41c64e6du64.wrapping_mul(PRNG_State as u64) as i64;
-        *(&mut t as *mut i64 as *mut u32).offset(1isize) <<= 16i32;
-        t = (t as u64).wrapping_add(0xffff00003039u64) as i64 as i64;
-        PRNG_State = t as u32;
-        *p = *(&mut t as *mut i64 as *mut u32).offset(1isize) & 0xffff0000u32 | t as u32 >> 16i32;
-        p = p.offset(-1isize);
+
+    table[0] = 0;
+    table[1] = 103;
+    unsafe {
+        p = table.as_mut_ptr().offset(251);
+    }
+    count = 250;
+    i = 0;
+    while i < 250 {
+        unsafe {
+            t = 0x41c64e6du64.wrapping_mul(PRNG_State as u64) as i64;
+            *(&mut t as *mut i64 as *mut u32).offset(1isize) <<= 16i32;
+            t = (t as u64).wrapping_add(0xffff00003039u64) as i64 as i64;
+            PRNG_State = t as u32;
+            *p = *(&mut t as *mut i64 as *mut u32).offset(1isize) & 0xffff0000u32
+                | t as u32 >> 16i32;
+            p = p.offset(-1isize);
+        }
         i += 1
     }
     a = 0xffffffffu32;
     k = 0x80000000u32;
-    q = table.offset(5isize) as *mut i32;
+    unsafe {
+        q = table.as_mut_ptr().offset(5) as *mut i32;
+    }
     loop {
-        b = *q;
-        *q = (k | a & b as u32) as i32;
-        q = q.offset(7isize);
+        unsafe {
+            b = *q;
+            *q = (k | a & b as u32) as i32;
+            q = q.offset(7isize);
+        }
         k >>= 1i32;
         a >>= 1i32;
         if !(0 != k) {
@@ -86,7 +99,7 @@ pub fn decrypt(Input: &mut [u8]) {
     let mut Table: [u32; 256] = [0; 256];
     let mut P: *mut u32 = Input.as_mut_ptr() as *mut u32;
     unsafe {
-        prng_init(Table.as_mut_ptr(), 1234567890u32);
+        prng_init(&mut Table, 1234567890u32);
     }
     Table[254usize] = 0i32 as u32;
     let fresh0 = P;
@@ -94,7 +107,7 @@ pub fn decrypt(Input: &mut [u8]) {
         P = P.offset(1);
     }
     unsafe {
-        prng_init(Table.as_mut_ptr(), *fresh0);
+        prng_init(&mut Table, *fresh0);
     }
     Len = (Input.len() - 4) / 4;
     I = 0;
